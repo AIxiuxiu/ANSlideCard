f12905e90cb6244f86f5a9406a2773c65ea97cf2
AIxiuxiu <1967260047@qq.com>
2016-11-30 15:46:49 +0800

    Initial Commit

---
+++ ANSlideCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANSlideCard
+//
+//  Created by XAYQ-FanXL on 16/11/30.
+//  Copyright © 2016年 AN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ANSlideCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ANSlideCard
+//
+//  Created by XAYQ-FanXL on 16/11/30.
+//  Copyright © 2016年 AN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
